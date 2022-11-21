778616c3cc43fb1541e48310c71ed2f2125fa7db
publicsaito3110 <public.business.saito3110@gmail.com>
2022-11-21 19:52:40 +0900

    rename: Rest1Controllerへ変更

--- src/main/java/com/springwebflux/controller/Rest1Controller.java
+++ src/main/java/com/springwebflux/controller/Rest1Controller.java
@@ -7,18 +7,22 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-public class SimpleRestController {
+public class Rest1Controller {
 
 
 	/**
 	 * Monoを使ったシンプルなRestAPI
 	 *
+	 * <p>単一処理:<br>
+	 * リクエスト -> [処理] -> レスポンス
+	 * </p>
+	 *
 	 * 返却値: Hello World!
 	 *
 	 * @return Mono
 	 */
-	@RequestMapping("/hello11")
-	public Mono<String> hello11() {
+	@RequestMapping("/rest11")
+	public Mono<String> rest11() {
 
 		// Monoは単一のオブジェクトのみStreamできる
 		return Mono.just("Hello World!");
@@ -28,12 +32,15 @@ public class SimpleRestController {
 	/**
 	 * Fluxを使ったシンプルなRestAPI
 	 *
+	 * <p>単一処理:<br>
+	 * リクエスト -> [処理] -> レスポンス
+	 * </p>
 	 * 返却値: Hello World! WebFlux
 	 *
-	 * @return Mono
+	 * @return Flux
 	 */
-	@RequestMapping("/hello21")
-	public Flux<String> hello21() {
+	@RequestMapping("/rest12")
+	public Flux<String> rest12() {
 
 		// Fluxは複数のオブジェクトをStreamできる
 		return Flux.just("Hello World!", "WebFlux");
@@ -43,12 +50,16 @@ public class SimpleRestController {
 	/**
 	 * Fluxを使ったRestAPI
 	 *
+	 * <p>単一処理:<br>
+	 * リクエスト -> [処理] -> レスポンス
+	 * </p>
+	 *
 	 * 返却値: [0, 1, 2, 3, 4]
 	 *
-	 * @return Mono
+	 * @return Flux
 	 */
-	@RequestMapping("/hello22")
-	public Flux<Integer> hello22() {
+	@RequestMapping("/rest13")
+	public Flux<Integer> rest13() {
 
 		// Fluxを使ったStream
 		Flux<Integer> flux = Flux
